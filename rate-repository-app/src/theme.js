27a175a6590e87f1425c1eea774c3c602029e430
Haseeb <haseeb.shaikh@helsinki.fi>
2025-07-21 16:46:42 +0300

    Background fixed for 10.4

--- rate-repository-app/src/theme.js
+++ rate-repository-app/src/theme.js
@@ -3,7 +3,7 @@ const theme = {
     textPrimary: '#24292e',
     textSecondary: '#586069',
     primary: '#0366d6',
-    appBarBackground: '#24292e', // <-- Add this line
+    appBarBackground: '#24292e',
   },
   fontSizes: {
     body: 14,
